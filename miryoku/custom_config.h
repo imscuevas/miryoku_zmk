e1f46e3b192f5df7bbba7ca727b1366cfcce622f
Sinhue Cuevas <imscuevas@gmail.com>
2025-12-16 20:13:08 -0600

    Add default configuration defines for Miryoku

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_EXTRA_DEFAULT
+#define MIRYOKU_NAV_VI
+#define MIRYOKU_CLIPBOARD_DEFAULT
+#define MIRYOKU_LAYERS_DEFAULT
+#define MIRYOKU_MAPPING_DEFAULT
